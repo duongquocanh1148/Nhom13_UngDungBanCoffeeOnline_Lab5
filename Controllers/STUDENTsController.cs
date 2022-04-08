69b7cba3335633e771127006fe1de29471edbd1a
duongquocanh1148 <duongquocanh1148@gmailcom>
2022-04-08 16:23:37 +0700

    Done

---
+++ Controllers/STUDENTsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nhom13_UngDungBanCoffeeOnline.Models;
+
+namespace Nhom13_UngDungBanCoffeeOnline.Controllers
+{
+    public class STUDENTsController : Controller
+    {
+        private SchoolEntities db = new SchoolEntities();
+
+        // GET: STUDENTs
+        public ActionResult Index()
+        {
+            return View(db.STUDENTs.ToList());
+        }
+
+        // GET: STUDENTs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: STUDENTs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "st_id,st_name,st_username,st_password,st_dob,st_gender,st_address,st_email,st_phone,fac_id")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.STUDENTs.Add(sTUDENT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: STUDENTs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "st_id,st_name,st_username,st_password,st_dob,st_gender,st_address,st_email,st_phone,fac_id")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sTUDENT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: STUDENTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            db.STUDENTs.Remove(sTUDENT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
